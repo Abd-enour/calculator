1a2e1e33d0d085c82e1c9fb78d3e6c6faabb09aa
abdennour <abdennour.oussaid9@gmail.com>
2022-08-28 15:19:36 +0100

    done with the plus, minus ,multiply, and divide button

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ let divide = document.getElementById("divide");
 let openParenthises = document.getElementById("open-parenthises");
 let closeParenthises = document.getElementById("close-parenthises");
 let clBtn = document.getElementById("clBtn");
+
+
 let a =Number(firstScreen.textContent);
 let b=Number(lastScreen.textContent);
 
@@ -34,7 +36,29 @@ plus.onclick=()=>{
             firstScreen.textContent="";
             lastScreen.textContent=output + " +"
       }
+      else if(a[1]==="*"){
+         if(firstScreen.textContent!==""){
+         let output= Number(a[0]) * Number(firstScreen.textContent);
+            firstScreen.textContent="";
+            lastScreen.textContent=output + " +";
+         }else{
+            return lastScreen.textContent= a[0]+" +";
+         }
+      }
+      else if(a[1]==="/"){
+         if (firstScreen.textContent!=="" && Number(firstScreen.textContent)!==0){
+         let output= Number(a[0]) / Number(firstScreen.textContent);
+            firstScreen.textContent="";
+            lastScreen.textContent=output + " +";
+         }else{
+            lastScreen.textContent="";
+            firstScreen.textContent="ERROR:Can't divide by 0"
+         }
+      }
    }else{
+      if(firstScreen.textContent ==="0" && lastScreen.textContent===""){
+         return ;
+      }
       let output= Number(a[0]) +Number(firstScreen.textContent);
       firstScreen.textContent="";
       lastScreen.textContent=output + " +"
@@ -52,21 +76,119 @@ minus.onclick=()=>{
       else if(a[1]==="+"){
          let output= Number(a[0]) +Number(firstScreen.textContent);
          firstScreen.textContent="";
-         lastScreen.textContent=output + " +"
+         lastScreen.textContent=output + " -"
+      }else if(a[1]==="*"){if(firstScreen.textContent!==""){
+         let output= Number(a[0]) * Number(firstScreen.textContent);
+            firstScreen.textContent="";
+            lastScreen.textContent=output + " +";
+         }else{
+            return lastScreen.textContent= a[0]+" +";
+         }
+      }else if(a[1]==="/"){
+         if(firstScreen.textContent!=="" && Number(firstScreen.textContent)!==0){
+            let output= Number(a[0])/Number(firstScreen.textContent);
+            firstScreen.textContent="";
+            lastScreen.textContent=output+" /";
+         }else{
+            lastScreen.textContent="";
+            firstScreen.textContent="ERROR:can't divide by 0";
+         }
       }
       
    }else{
-   let output= Number(a[0]) - Number(firstScreen.textContent);
-   firstScreen.textContent="";
-   lastScreen.textContent=output + " -";
+      if(firstScreen.textContent ==="0" && lastScreen.textContent===""){
+         return ;
+      }
+      let output= Number(firstScreen.textContent) - Number(a[0]);
+      firstScreen.textContent="";
+      lastScreen.textContent=output + " -";
    }
 }
 
 multiply.onclick=()=>{
    let a = lastScreen.textContent.split(" ");
-   let output= (Number(a[0]) * Number(firstScreen.textContent));
-   firstScreen.textContent="";
-   lastScreen.textContent=output + " *"
+   if(a.length===2){
+      if(a[1]==="+"){
+         let output= Number(a[0]) +Number(firstScreen.textContent);
+         firstScreen.textContent="";
+         lastScreen.textContent=output + " *"
+      }
+      else if (a[1]==="-") {
+         let output= Number(a[0]) -Number(firstScreen.textContent);
+         firstScreen.textContent="";
+         lastScreen.textContent=output + " *"   
+      }
+      else if (a[1]==="*") {
+         let output= Number(a[0]) *Number(firstScreen.textContent);
+         firstScreen.textContent="";
+         lastScreen.textContent=output + " *"
+      }
+      else if (a[1]==="/") {
+         if(firstScreen.textContent!=="" && Number(firstScreen.textContent)!==0){
+            let output= Number(a[0])/Number(firstScreen.textContent);
+            firstScreen.textContent="";
+            lastScreen.textContent=output+" /";
+         }else{
+            lastScreen.textContent="";
+            firstScreen.textContent="ERROR:can't divide by 0";
+         }
+      }
+   }else{
+   if(firstScreen.textContent ==="0" && lastScreen.textContent===""){
+      return ;
+   } else{ 
+      if(lastScreen.textContent!=="" && firstScreen.textContent===""){
+         return ;
+      }else if(lastScreen.textContent==="" && firstScreen.textContent!==""){
+         lastScreen.textContent=firstScreen.textContent + " *";
+         firstScreen.textContent="";
+      }
+      else{
+         let output= (Number(a[0]) * Number(firstScreen.textContent));
+         firstScreen.textContent="";
+         lastScreen.textContent=output + " *";
+      }
+}
+   }
 }
 
-divide.onclick=()=>{}
+divide.onclick=()=>{
+   let a = lastScreen.textContent.split(" ");
+   if(a.length===2){
+      if (a[1]==="+") {
+         let output= Number(a[0]) +Number(firstScreen.textContent);
+         firstScreen.textContent="";
+         lastScreen.textContent=output + " /";
+      }else if(a[1]==="-"){
+         let output= Number(a[0]) - Number(firstScreen.textContent);
+         firstScreen.textContent="";
+         lastScreen.textContent=output + " /";
+      }else if(a[1]==="*"){
+         let output= Number(a[0]) *Number(firstScreen.textContent);
+         firstScreen.textContent="";
+         lastScreen.textContent=output + " /"
+      }else if(a[1]==="/"){
+         if(firstScreen.textContent!=="" && Number(firstScreen.textContent)!==0){
+            let output= Number(a[0])/Number(firstScreen.textContent);
+            firstScreen.textContent="";
+            lastScreen.textContent=output+" /";
+         }else{
+            lastScreen.textContent="";
+            firstScreen.textContent="ERROR:can't divide by 0";
+         }
+      }
+
+   }else{
+      if(lastScreen.textContent==="" && firstScreen.textContent===""){
+         return ;
+      }else if(firstScreen.textContent!==""){
+         lastScreen.textContent=firstScreen.textContent+ " /";
+         firstScreen.textContent="";
+      }
+   }
+}
+
+clBtn.onclick=()=>{
+   firstScreen.textContent="0";
+   lastScreen.textContent="";
+}
